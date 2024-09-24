7215ae4bce89f0e97bd55561c4f02a43e9d478aa
anjan-cs98 <122563582+anjan-cs98@users.noreply.github.com>
2024-09-24 18:22:47 +0530

    small change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,7 +15,7 @@ export default {
 					secondary: "#FFFFFF", // White
 					accent: "#7FC15E", // LinkedIn Green (for accents)
 					neutral: "#000000", // Black (for text)
-					"base-100": "#F3F2EF", // Light Gray (background)
+				       base: "#F3F2EF", // Light Gray (background)
 					info: "#5E5E5E", // Dark Gray (for secondary text)
 					success: "#057642", // Dark Green (for success messages)
 					warning: "#F5C75D", // Yellow (for warnings)
